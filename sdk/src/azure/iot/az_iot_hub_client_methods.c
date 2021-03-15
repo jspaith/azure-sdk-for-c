50d7d4facd42f68250d0a4047e73ae2bfbb7f80e
John Spaith <jspaith@windows.microsoft.com>
2021-03-15 09:55:29 -0600

    Further optimization

--- sdk/src/azure/iot/az_iot_hub_client_methods.c
+++ sdk/src/azure/iot/az_iot_hub_client_methods.c
@@ -23,6 +23,61 @@ static const az_span methods_response_topic_properties = AZ_SPAN_LITERAL_FROM_ST
 static const az_span command_separator = AZ_SPAN_LITERAL_FROM_STR("*");
 
 
+AZ_NODISCARD az_result az_iot_hub_get_request_id_and_name(
+    az_span received_topic,
+    az_span* out_request_id,
+    az_span* out_name,
+    int32_t* out_index
+)
+{
+    int32_t index = az_span_find(received_topic, methods_topic_prefix);
+    
+    if (index == -1)
+    {
+      return AZ_ERROR_IOT_TOPIC_NO_MATCH;
+    }
+    
+    if (_az_LOG_SHOULD_WRITE(AZ_LOG_MQTT_RECEIVED_TOPIC))
+    {
+      _az_LOG_WRITE(AZ_LOG_MQTT_RECEIVED_TOPIC, received_topic);
+    }
+    
+    received_topic = az_span_slice(
+        received_topic, index + az_span_size(methods_topic_prefix), az_span_size(received_topic));
+    
+    index = az_span_find(received_topic, methods_topic_filter_suffix);
+    
+    if (index == -1)
+    {
+      return AZ_ERROR_IOT_TOPIC_NO_MATCH;
+    }
+    
+    received_topic = az_span_slice(
+        received_topic,
+        index + az_span_size(methods_topic_filter_suffix),
+        az_span_size(received_topic));
+    
+    index = az_span_find(received_topic, methods_response_topic_properties);
+    
+    if (index == -1)
+    {
+      return AZ_ERROR_IOT_TOPIC_NO_MATCH;
+    }
+    
+    *out_name = az_span_slice(received_topic, 0, index);
+    *out_request_id = az_span_slice(
+        received_topic,
+        index + az_span_size(methods_response_topic_properties),
+        az_span_size(received_topic));
+
+    if (out_index != NULL)
+    {
+        *out_index = index;
+    }
+
+    return AZ_OK;
+}
+    
 AZ_NODISCARD az_result az_iot_hub_client_methods_parse_received_topic(
     az_iot_hub_client const* client,
     az_span received_topic,
@@ -35,47 +90,14 @@ AZ_NODISCARD az_result az_iot_hub_client_methods_parse_received_topic(
 
   (void)client;
 
-  int32_t index = az_span_find(received_topic, methods_topic_prefix);
+  _az_RETURN_IF_FAILED(az_iot_hub_get_request_id_and_name(
+    received_topic,
+    &out_request->request_id,
+    &out_request->name,
+    NULL)
+    );
 
-  if (index == -1)
-  {
-    return AZ_ERROR_IOT_TOPIC_NO_MATCH;
-  }
-
-  if (_az_LOG_SHOULD_WRITE(AZ_LOG_MQTT_RECEIVED_TOPIC))
-  {
-    _az_LOG_WRITE(AZ_LOG_MQTT_RECEIVED_TOPIC, received_topic);
-  }
-
-  received_topic = az_span_slice(
-      received_topic, index + az_span_size(methods_topic_prefix), az_span_size(received_topic));
-
-  index = az_span_find(received_topic, methods_topic_filter_suffix);
-
-  if (index == -1)
-  {
-    return AZ_ERROR_IOT_TOPIC_NO_MATCH;
-  }
-
-  received_topic = az_span_slice(
-      received_topic,
-      index + az_span_size(methods_topic_filter_suffix),
-      az_span_size(received_topic));
-
-  index = az_span_find(received_topic, methods_response_topic_properties);
-
-  if (index == -1)
-  {
-    return AZ_ERROR_IOT_TOPIC_NO_MATCH;
-  }
-
-  out_request->name = az_span_slice(received_topic, 0, index);
-  out_request->request_id = az_span_slice(
-      received_topic,
-      index + az_span_size(methods_response_topic_properties),
-      az_span_size(received_topic));
-
-  return AZ_OK;
+    return AZ_OK;
 }
 
 AZ_NODISCARD az_result az_iot_hub_client_methods_response_get_publish_topic(
@@ -120,6 +142,40 @@ AZ_NODISCARD az_result az_iot_hub_client_methods_response_get_publish_topic(
 }
 
 
+AZ_NODISCARD az_result az_iot_hub_client_commands_parse_received_topic(
+    az_iot_hub_client const* client,
+    az_span received_topic,
+    az_iot_hub_client_command_request* out_request)
+{
+  _az_PRECONDITION_NOT_NULL(client);
+  _az_PRECONDITION_VALID_SPAN(received_topic, 1, false);
+  _az_PRECONDITION_NOT_NULL(out_request);
+
+  (void)client;
+  int32_t index;
+
+  _az_RETURN_IF_FAILED(az_iot_hub_get_request_id_and_name(
+    received_topic,
+    &out_request->request_id,
+    &out_request->command_name,
+    &index)
+    );
+
+  int32_t command_separator_index = az_span_find(received_topic, command_separator);
+  if (command_separator_index > 0)
+  {
+    out_request->component_name = az_span_slice(received_topic, 0, command_separator_index);
+    out_request->command_name = az_span_slice(received_topic, command_separator_index + 1, index);
+  }
+  else
+  {
+    out_request->component_name = AZ_SPAN_EMPTY;
+    out_request->command_name = az_span_slice(received_topic, 0, index);
+  }
+
+  return AZ_OK;
+}
+
 AZ_NODISCARD az_result az_iot_hub_client_commands_response_get_publish_topic(
     az_iot_hub_client const* client,
     az_span request_id,
@@ -135,64 +191,5 @@ AZ_NODISCARD az_result az_iot_hub_client_commands_response_get_publish_topic(
       mqtt_topic,
       mqtt_topic_size,
       out_mqtt_topic_length);
-}
-
-
-AZ_NODISCARD az_result az_iot_hub_client_commands_parse_received_topic(
-    az_iot_hub_client const* client,
-    az_span received_topic,
-    az_iot_hub_client_command_request* out_request)
-{
-  _az_PRECONDITION_NOT_NULL(client);
-  _az_PRECONDITION_VALID_SPAN(received_topic, 1, false);
-  _az_PRECONDITION_NOT_NULL(out_request);
-
-  (void)client;
-
-  int32_t index = az_span_find(received_topic, methods_topic_prefix);
-
-  if (index == -1)
-  {
-    return AZ_ERROR_IOT_TOPIC_NO_MATCH;
-  }
-
-  _az_LOG_WRITE(AZ_LOG_MQTT_RECEIVED_TOPIC, received_topic);
-
-  received_topic
-      = az_span_slice_to_end(received_topic, index + az_span_size(methods_topic_prefix));
-
-  index = az_span_find(received_topic, methods_topic_filter_suffix);
-
-  if (index == -1)
-  {
-    return AZ_ERROR_IOT_TOPIC_NO_MATCH;
-  }
-
-  received_topic
-      = az_span_slice_to_end(received_topic, index + az_span_size(methods_topic_filter_suffix));
-
-  index = az_span_find(received_topic, methods_response_topic_properties);
-
-  if (index == -1)
-  {
-    return AZ_ERROR_IOT_TOPIC_NO_MATCH;
-  }
-
-  out_request->request_id = az_span_slice_to_end(
-      received_topic, index + az_span_size(methods_response_topic_properties));
-
-  int32_t command_separator_index = az_span_find(received_topic, command_separator);
-  if (command_separator_index > 0)
-  {
-    out_request->component_name = az_span_slice(received_topic, 0, command_separator_index);
-    out_request->command_name = az_span_slice(received_topic, command_separator_index + 1, index);
-  }
-  else
-  {
-    out_request->component_name = AZ_SPAN_EMPTY;
-    out_request->command_name = az_span_slice(received_topic, 0, index);
-  }
-
-  return AZ_OK;
 }
 
--- sdk/src/azure/iot/az_iot_hub_client_telemetry.c
+++ sdk/src/azure/iot/az_iot_hub_client_telemetry.c
@@ -19,6 +19,7 @@ static const az_span telemetry_topic_suffix = AZ_SPAN_LITERAL_FROM_STR("/message
 static const az_span telemetry_component_prop_span = AZ_SPAN_LITERAL_FROM_STR("$.sub");
 
 
+
 AZ_NODISCARD az_result az_iot_hub_client_telemetry_get_publish_topic(
     az_iot_hub_client const* client,
     az_iot_message_properties const* properties,
@@ -26,55 +27,14 @@ AZ_NODISCARD az_result az_iot_hub_client_telemetry_get_publish_topic(
     size_t mqtt_topic_size,
     size_t* out_mqtt_topic_length)
 {
-  _az_PRECONDITION_NOT_NULL(client);
-  _az_PRECONDITION_NOT_NULL(mqtt_topic);
-  _az_PRECONDITION(mqtt_topic_size > 0);
-
-  const az_span* const module_id = &(client->_internal.options.module_id);
-
-  az_span mqtt_topic_span = az_span_create((uint8_t*)mqtt_topic, (int32_t)mqtt_topic_size);
-  int32_t required_length = az_span_size(telemetry_topic_prefix)
-      + az_span_size(client->_internal.device_id) + az_span_size(telemetry_topic_suffix);
-  int32_t module_id_length = az_span_size(*module_id);
-  if (module_id_length > 0)
-  {
-    required_length += az_span_size(telemetry_topic_modules_mid) + module_id_length;
-  }
-  if (properties != NULL)
-  {
-    required_length += properties->_internal.properties_written;
-  }
-
-  _az_RETURN_IF_NOT_ENOUGH_SIZE(
-      mqtt_topic_span, required_length + (int32_t)sizeof(null_terminator));
-
-  az_span remainder = az_span_copy(mqtt_topic_span, telemetry_topic_prefix);
-  remainder = az_span_copy(remainder, client->_internal.device_id);
-
-  if (module_id_length > 0)
-  {
-    remainder = az_span_copy(remainder, telemetry_topic_modules_mid);
-    remainder = az_span_copy(remainder, *module_id);
-  }
-
-  remainder = az_span_copy(remainder, telemetry_topic_suffix);
-
-  if (properties != NULL)
-  {
-    remainder = az_span_copy(
-        remainder,
-        az_span_slice(
-            properties->_internal.properties_buffer, 0, properties->_internal.properties_written));
-  }
-
-  az_span_copy_u8(remainder, null_terminator);
-
-  if (out_mqtt_topic_length)
-  {
-    *out_mqtt_topic_length = (size_t)required_length;
-  }
-
-  return AZ_OK;
+    return az_iot_client_telemetry_with_component_get_publish_topic(
+        client,
+        AZ_SPAN_EMPTY,
+        (az_iot_message_properties*)properties, // TODO: To make compile since we changed const in API signature.  To discuss.
+        mqtt_topic,
+        mqtt_topic_size,
+        out_mqtt_topic_length
+    );
 }
 
 
