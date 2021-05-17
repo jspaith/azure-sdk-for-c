2687436b3c36c6de3320c138216471996e2c71cd
John Spaith <jspaith@windows.microsoft.com>
2021-05-17 13:48:18 -0600

    Refactor common code into paho_iot_pnp_sample_common.c

---
+++ sdk/samples/iot/paho_iot_pnp_sample_common.h
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#ifndef PAHO_IOT_PNP_SAMPLE_COMMON
+#define PAHO_IOT_PNP_SAMPLE_COMMON
+
+#define MQTT_TIMEOUT_RECEIVE_MAX_MESSAGE_COUNT 100
+#define MQTT_TIMEOUT_RECEIVE_MS (8 * 1000)
+#define MQTT_TIMEOUT_DISCONNECT_MS (10 * 1000)
+
+
+extern MQTTClient mqtt_client;
+extern az_iot_hub_client hub_client;
+
+void paho_iot_pnp_sample_device_implement();
+
+#endif // PAHO_IOT_PNP_SAMPLE_COMMON
+
